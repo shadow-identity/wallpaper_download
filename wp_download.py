0eecccdab9040769ef2b1b24745605374d4fb3bc
Pavel Nedrigaylov <pavel.nedr@gmail.com>
2012-10-31 13:49:04 +0400

    removed debug variable (algorythm changed to use __debug__ constant fixed issue with exception in download()

--- wp_download.py
+++ wp_download.py
@@ -7,45 +7,45 @@ http://www.australiangeographic.com.au and store it to local storage
 import urllib2, logging
 from bs4 import BeautifulSoup
 
-logging.basicConfig(level = logging.DEBUG) 
+if __debug__: logging.basicConfig(level = logging.DEBUG) 
 
-debug = True
 
-def getimg(url, debug=False):
+def getimg(url):
     """ Open wallpaper's page, find url of hires picture
     in <div id="content"> --> children <p><a>. 
     Get URL from <a href="...">. """
     # where to save downloaded wallpapers
-    local_path = '/home/nedr/Изображения/wp/au/'
-    logging.debug('opening %s' % url)
+    local_path = '/home/nedr/Изображения/wp/au/temp/'
+    logging.debug('opening %s', url)
     soup = BeautifulSoup(urllib2.urlopen(url))
     try:
         img_url = soup.find("div", id="content").p.a.get('href')
-        logging.debug(u'url of img: %s' % img_url)
+        logging.debug(u'url of img: %s', img_url)
         logging.debug(img_url[img_url.rfind('/') + 1:])
         logging.debug('/home/nedr/progs/test/wallpapers' 
                       + img_url[img_url.rfind('/'):])
-        if not debug:
+        if not __debug__:
+            logging.info('downloading %s', img_url)
             download(img_url, local_path
                      + img_url[img_url.rfind('/'):])
     except urllib2.URLError:
-        logging.error('URLError while parsing %s' % url)
+        logging.error('URLError while parsing %s', url)
     return
 
 
 def download(url, dest):
     """Download file from url and save it as dest. """
     try:
-        s = urllib2.urlopen(url)
-        content = s.read()
-        with open(dest,'wb') as d:
-            d.write(content)
+        wallpaper_net = urllib2.urlopen(url)
+        content = wallpaper_net.read()
+        with open(dest,'wb') as wallpaper_file:
+            wallpaper_file.write(content)
     except urllib2.URLError:
-        logging.error('ERLError while saving img from %s' % url)
+        logging.error('ERLError while saving img from %s', url)
     except IOError:
-        logging.error('IOError while saving %s in %s' % (url, dest))
-    finally:
-        s.close()
+        logging.error('IOError while saving %s in %s', url, dest)
+    else:
+        wallpaper_net.close
     return
     
 
@@ -60,14 +60,10 @@ def main():
         # if url is not absolute
         if not link_str.startswith('http'):
             link_str = 'http://www.australiangeographic.com.au' + link_str
-        getimg(link_str, debug)
+        getimg(link_str)
         links += 1
 
-    logging.info('links to img pages found: %d' % (links))
+    logging.info('links to img pages found: %d', links)
 
 
-if debug:
-    #getimg('http://www.australiangeographic.com.au/journal/images-of-australia-coneflower.htm', debug)
-    main()
-else:
-    main()
+main()
