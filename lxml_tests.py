082b38fc8c02b1e772c47e29d99469011f2e6a20
Pavel Nedrigaylov <pavel.nedr@gmail.com>
2013-03-20 23:16:04 +0400

    added get_original_image_url() save_image
    imfo about exif saving
    
    removed unused comments

--- lxml_tests.py
+++ lxml_tests.py
@@ -2,15 +2,17 @@
 
 
 import urllib
+import urllib2
 import lxml.html
 
 url = 'http://www.australiangeographic.com.au/journal/wallpaper'
 html_file = '/home/nedr/progs/wp_download/gallery.html'
 site_prefix = 'http://www.australiangeographic.com.au'
+path_to_file = '/home/nedr/progs/wp_download/wp/'
 doc_file = open(html_file)
 doc = lxml.html.document_fromstring(doc_file.read())
-#html = urllib.urlopen(url).read()
-#doc = lxml.html.document_fromstring(html)
+html = urllib.urlopen(url).read()
+doc = lxml.html.document_fromstring(html)
 
 
 class Image():
@@ -23,33 +25,39 @@ class Image():
         #TODO: remove author if None
         self.name = image[1].text
 
+    def get_original_image_url(self):
+        prewiev_html = urllib.urlopen(self.prewiev_url).read()
+        prewiev_doc = lxml.html.document_fromstring(prewiev_html)
+        self.image_url = prewiev_doc.xpath(
+                        '//*[@id="content"]/p/a')[0].attrib['href']
+        print self.image_url
+
+    def save_image(self):
+        """Download file from url and save it as dest."""
+        """try:"""
+        image_pointer = urllib2.urlopen(self.image_url)
+        image = image_pointer.read()
+        with open(path_to_file + self.name + '.jpg', 'wb') as wallpaper_file:
+            wallpaper_file.write(image)
+        """except urllib2.URLError:
+            logging.error('ERLError while saving img from %s', url)
+        except IOError:
+            logging.error('IOError while saving %s in %s', url, dest)
+        else:"""
+        image_pointer.close
+
+    def edit_exit(self):
+        #TODO: write exif support
+        """ pyexiv2
+            see http://stackoverflow.com/questions/ \
+            765396/exif-manipulation-library-for-python
+            0x013b    315    Image    Exif.Image.Artist
+            0x8298    33432    Image    Exif.Image.Copyright
+        """
 
 page = Image()
-
+#TODO: add exceptions
 for image in doc.xpath('//*[@id="content"]/table/tr/td/div'):
     page.get_data_from_gallery(image)
-    print page.author, page.name, page.prewiev_url
-
-"""doc.xpath('//*[@id="content"]/table/tr/td/div')[0][1][1].text
-'David Bristow'
-
-doc.xpath('//*[@id="content"]/table/tr/td/div')[0][1].text
-'Green sea turtle, WA'
-
-doc.xpath('//*[@id="content"]/table/tr/td/div')[0][0][0].attrib['href']
-'http://www.australiangeographic.com.au/journal/images-of-australia-green-sea-turtle.htm'
-"""
-    #page.prewiev_url = image.attrib["href"]
-
-#first_string = xml.xpath('.//*[@id="content"]/table/')
-#              '//*[@id="content"]/table/tbody/tr[1]/td[1]/div/p[2]/em/text()')
-
-#txt1 = xmldata.xpath('/html/body/span[@class="simple_text"]/text()[1]')
-# Находим тег «span», у которого аттрибут «class» равен значению «simple_text»
-# и с помощью функции text() получаем текст элемента
-#txt2 = xmldata.xpath(
-#     '/html/body/span[@class="cyrillic_text"]/following-sibling::text()[1]')
-# Находим тег «span», у которого аттрибут «class» равен значению
-# «cyrillic_text» и получаем следующий за ним текст с помощью following-sibling
-# (получаем следующий в ветке элемент) и text()
-# Для получение значений использовался XPath
+    page.get_original_image_url()
+    page.save_image()
